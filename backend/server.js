b68be4e64d847924cf1e45bade0828090fb81e24
JanmejayR <JayRathore.1010@gmail.com>
2024-09-17 01:56:40 +0530

    serving static files

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 dotenv.config();
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
+const __dirname = path.resolve();
+
 const app = express();
 app.use(cors({
     credentials:true,
@@ -15,6 +18,13 @@ app.use(express.json())
 
 app.use('/api/auth' , authRoutes)
 app.use('/api/user', userRoutes)
+
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
+
+app.get("*" , (req,res)=> {
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"
+    ))
+})
 
 
 app.listen(process.env.PORT , ()=>{
